d6f94145341e3cd5380d48beec9dba7607773e86
이상헌 <sangheonlee@isangheon-ui-MacBookPro.local>
2025-03-26 19:28:25 +0900

    first commit

---
+++ src/main/java/com/musinsa/backend/MusinsaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musinsa/backend/MusinsaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
